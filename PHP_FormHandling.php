485ad995eb9ed71c0f5e2ed4d69261de6c0a5eaa
Swetha Janardhan <swethaiyer26@gmail.com>
2020-01-07 16:26:32 +0530

    code changes & commit

---
+++ PHP_FormHandling.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="welcome.php" method ="post">
+name: <input type ="text" name ="name"><br>
+email:<input type="text" name="email"><br>
+<input type ="submit">
+</form>
+</body>
+</html>
---
+++ PHP_FormHandlingV1.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="welcome_get.php" method ="get">
+name: <input type ="text" name ="name"><br>
+email:<input type="text" name="email"><br>
+<input type ="submit">
+</form>
+</body>
+</html>
